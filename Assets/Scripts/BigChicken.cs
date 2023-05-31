ff845ccb1922202239add9ce3c8c1bec5db5c094
ayberkderingoz <ayberkderingoz@gmail.com>
2023-05-31 22:11:27 +0300

    Mother Chicken Updates

--- Assets/Scripts/BigChicken.cs
+++ Assets/Scripts/BigChicken.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UI;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class BigChicken : MonoBehaviour
 {
     private int gold { get; set; } = 100;
     
     
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            var wormCount = CountUI.Instance.GetCount(CountUI.CountType.Worm);
+            var wormCount = ScoreManager.Instance.GetScore(ScoreManager.ScoreType.Worm);
             if (wormCount == 0)
             {
                 return;
@@ -21,9 +23,19 @@ public class BigChicken : MonoBehaviour
             
             var multipliedGold = gold * wormCount;
             
-            CountUI.Instance.UpdateCount(CountUI.CountType.Gold,multipliedGold);
-            CountUI.Instance.ResetCount(CountUI.CountType.Worm);
+            var currentGold = ScoreManager.Instance.GetScore(ScoreManager.ScoreType.Gold);
+            ScoreManager.Instance.UpdateScore(ScoreManager.ScoreType.Gold,currentGold+multipliedGold);
+            
+            ScoreManager.Instance.ResetScore(ScoreManager.ScoreType.Worm);
 
+        }
+        else if (other.CompareTag("Worker"))
+        {
+            if(other.gameObject.GetComponent<WorkerChicken>().IsCarrying())
+            {
+                var currentGold = ScoreManager.Instance.GetScore(ScoreManager.ScoreType.Gold);
+                ScoreManager.Instance.UpdateScore(ScoreManager.ScoreType.Gold,currentGold+gold);
+            }
         }
     }
 }
